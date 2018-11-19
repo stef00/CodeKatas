37b8004f79ada1a0805ac9d7a6d2837162da26f7
stef00 <stefanogechele@gmail.com>
2018-11-19 18:12:49 +0100

    final

--- rotate-for-a-maz.js
+++ rotate-for-a-maz.js
@@ -1,29 +1,13 @@
 function maxRot(n) {
-
     let arr = n.toString().split("");
-    
-    //1
-    var firstElement = arr.shift();
-    arr.push(firstElement);
-    var primo = arr.join("");
-
-    //2
-    let rimosso = arr.splice(1,1);
-    arr.push(rimosso[0]);
-    
-    var secondo = arr.join("");
-    //3
-    rimosso = arr.splice(2,1);
-    arr.push(rimosso[0]);
-
-    var terzo = arr.join("");
-    //4
-    rimosso = arr.splice(3,1);
-    arr.push(rimosso[0]);
-    var quarto = arr.join("");
-
-    return Math.max(primo, secondo, terzo, quarto);
-   
+    let arr_pos =[n];
+    for (i = 0; i < arr.length -1; i++) {
+        let rimosso = arr.splice(i, 1);
+        arr.push(rimosso[0]);
+        arr_pos.push(arr.join(""));
+    }
+    console.log(arr_pos)
+    return Math.max(...arr_pos);
 }
 
 console.log(maxRot(38458215), 85821534);
