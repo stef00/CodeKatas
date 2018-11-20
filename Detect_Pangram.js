371d8944ca45ef83adf31f904b522abcd7932e13
stef00 <stefanogechele@gmail.com>
2018-11-20 15:12:55 +0100

    inizio

---
+++ Detect_Pangram.js
@@ -0,0 +1,9 @@
+function isPangram(string){
+    //...
+  }
+
+
+var string = "The quick brown fox jumps over the lazy dog."
+console.log(isPangram(string), true);
+var string = "This is not a pangram."
+console.log(isPangram(string), false);
