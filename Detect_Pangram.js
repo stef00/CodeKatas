616066a0f96ce052d52586aac657611f9cd7199a
stef00 <stefanogechele@gmail.com>
2018-11-20 16:22:10 +0100

    fine1

--- Detect_Pangram.js
+++ Detect_Pangram.js
@@ -1,6 +1,19 @@
-function isPangram(string){
-    //...
-  }
+function isPangram(string) {
+    let str = string.split("").filter(c => c != " " && c != ".");
+    let reg = "abcdefghijklmnopqrstuvwxyz".split("");
+    for (let i = 0; i < reg.length; i++) {
+        for (let j = 0; j < str.length; j++) {
+            let alfa = reg[i];
+            let stringa = str[j];
+            if (alfa == stringa) {
+                reg.splice(i,1, "");
+            }
+        }
+    }
+    if(!reg.join(""))
+        return true;
+    else return false;
+}
 
 
 var string = "The quick brown fox jumps over the lazy dog."
