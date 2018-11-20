cc5add1b7c809723a3487f77a03aff3ae7a3b2b7
stef00 <stefanogechele@gmail.com>
2018-11-20 17:22:58 +0100

    fine2

--- Lowest product of 4 consecutive numbers.js
+++ Lowest product of 4 consecutive numbers.js
@@ -3,6 +3,24 @@ function lowestProduct(input) {
     if (input.length < 4)
         return "Number is too small";
     let arr = input.split("");
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        let mol = 0;
+        if (i < arr.length - 3) {
+            mol = arr[i] * arr[i + 1] * arr[i + 2] * arr[i + 3];
+            mol_a.push(mol);
+        }
+    }
+    let min = Math.min(...mol_a);
+    return min;
+
+
+}
+function lowestProduct(input) {
+    let mol_a = [];
+    if (input.length < 4)
+        return "Number is too small";
+    let arr = input.split("");
     console.log(arr);
     return arr.reduce(function (_, _, ind, a) {
         let mol = 0;
@@ -15,26 +33,13 @@ function lowestProduct(input) {
         
         return Math.min(...mol_a);
     }, 0);
-
-    // for (let i = 0; i < arr.length; i++) {
-    //     const element = arr[i];
-    //     let mol = 0;
-    //     if (i < arr.length - 3) {
-    //         mol = arr[i] * arr[i + 1] * arr[i + 2] * arr[i + 3];
-    //         mol_a.push(mol);
-    //     }
-    // }
-    // let min = Math.min(...mol_a);
-    // return min;
-
-
 }
 
 
 
 
-// console.log(lowestProduct("123456789"), 24);
-// console.log(lowestProduct("234567899"), 120);
-// console.log(lowestProduct("2345611117899"), 1);
-// console.log(lowestProduct("333"), "Number is too small");
+console.log(lowestProduct("123456789"), 24);
+console.log(lowestProduct("234567899"), 120);
+console.log(lowestProduct("2345611117899"), 1);
+console.log(lowestProduct("333"), "Number is too small");
 console.log(lowestProduct("1234111"), 4, "Numbers should be consecutives");
