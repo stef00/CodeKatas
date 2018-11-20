6b3518ad2dd9ccea65e6941f878d11aeae0c4626
stef00 <stefanogechele@gmail.com>
2018-11-20 16:33:32 +0100

    inizio

---
+++ Lowest product of 4 consecutive numbers.js
@@ -0,0 +1,12 @@
+function lowestProduct(input) { 
+    
+}
+
+
+
+
+console.log(lowestProduct("123456789"),24); 
+console.log(lowestProduct("234567899"),120); 
+console.log(lowestProduct("2345611117899"),1);
+console.log(lowestProduct("333"),"Number is too small");
+console.log(lowestProduct("1234111"),4,"Numbers should be consecutives");        
