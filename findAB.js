7859c9429ef434dc98e9810ce5b9a7c48bd3cd3d
stef00 <stefanogechele@gmail.com>
2018-11-21 16:04:52 +0100

    inizio

---
+++ findAB.js
@@ -0,0 +1,15 @@
+function findAB(numbers,c){
+    //coding and coding...
+  
+  }
+
+console.log(findAB([1,2,3],3), [1,3]);
+console.log(findAB([1,2,3],6), [2,3]);
+console.log(findAB([1,2,3],7), null);
+console.log(findAB([1,2,3,6],6), [1,6]);
+console.log(findAB([1,2,3,4,5,6],15), [3,5]);
+console.log(findAB([0,0,2],4), null);
+console.log(findAB([0,0,2,2],4), [2,2]);
+console.log(findAB([-3,-2,-2,-1,0,1,2,3,4],4), [-2,-2]);
+console.log(findAB([-3,-2,-2,-1,0,1,2,3,4],0), [-3,0]);
+console.log(findAB([-3,-2,-1,0,1,2,3,4],4), [1,4]);
