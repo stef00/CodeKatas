78f2755cdccf988f3d18223332347e6dcc15be67
stef00 <stefanogechele@gmail.com>
2018-11-21 16:27:47 +0100

    inizio

---
+++ alphabetWar.js
@@ -0,0 +1,10 @@
+function alphabetWar(fight)
+{
+   return "Let's fight again!";
+}
+
+
+console.log( alphabetWar("z") , "Right side wins!" );
+console.log( alphabetWar("zdqmwpbs") , "Let's fight again!" );
+console.log( alphabetWar("zzzzs"), "Right side wins!" );
+console.log( alphabetWar("wwwwww"), "Left side wins!" );
