6c7d5dc9f2169a367fbf266f3d3a7597b7e8c75e
stef00 <stefanogechele@gmail.com>
2018-11-22 18:59:41 +0100

    inizio

---
+++ findParityNumber.js
@@ -0,0 +1,9 @@
+function findOutlier(integers){
+    //your code here
+  }
+  
+console.log(findOutlier([0, 1, 2]), 1)
+console.log(findOutlier([1, 2, 3]), 2)
+console.log(findOutlier([2,6,8,10,3]), 3)
+console.log(findOutlier([0,0,3,0,0]), 3)
+console.log(findOutlier([1,1,0,1,1]), 0)
