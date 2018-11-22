463714aec14c3a0ce3e4bab80977e3e468e78a5c
stef00 <stefanogechele@gmail.com>
2018-11-22 18:59:35 +0100

    fine

--- int32toIPV4.js
+++ int32toIPV4.js
@@ -1,7 +1,14 @@
 function int32ToIp(int32){
     //...
+    if(int32==0) return "0.0.0.0";
+    var arr=[];
+    var bin = int32.toString(2);
+    for (let i = 0; i < bin.length; i+=8) {
+        arr.push(bin.slice(i,i+8));
+    }
+    return arr.map(c=> parseInt(c , 2)).join(".");
+    
   }
-
 console.log( int32ToIp(2154959208), "128.114.17.104", "wrong ip for interger: 2154959208") 
 console.log( int32ToIp(0), "0.0.0.0", "wrong ip for integer: 0")
 console.log( int32ToIp(2149583361), "128.32.10.1", "wrong ip for integer: 2149583361")
