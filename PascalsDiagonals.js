90a15543a4af74a9c62ce45f340825b0ed78fdbd
stef00 <stefanogechele@gmail.com>
2018-11-28 18:36:20 +0100

    inizio

---
+++ PascalsDiagonals.js
@@ -0,0 +1,9 @@
+function generateDiagonal(n, l) {
+    // return an array containing the numbers in the nth diagonal of Pascal's triangle, to the specified length
+}
+
+
+
+console.log(generateDiagonal(2, 5), [1, 3, 6, 10, 15]);
+console.log(generateDiagonal(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(generateDiagonal(3, 7), [1, 4, 10, 20, 35, 56, 84]);
