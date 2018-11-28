f0fcd4154910e6b251cb4c64f48c0a7c9a2fdad7
stef00 <stefanogechele@gmail.com>
2018-11-28 19:38:57 +0100

    fine

--- PascalsDiagonals.js
+++ PascalsDiagonals.js
@@ -3,7 +3,7 @@ function generateDiagonal(row, len) {
     for (let k = 0; k < len; k++) {
         let num = rFact(row)/(rFact(k)*rFact(row-k));
         row++;
-        arr.push(num);
+        arr.push(Math.round(num));
     }
     return arr;
 }
@@ -18,5 +18,5 @@ function rFact(num) {
 
 
 console.log(generateDiagonal(2, 5), [1, 3, 6, 10, 15]);
-console.log(generateDiagonal(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-console.log(generateDiagonal(3, 7), [1, 4, 10, 20, 35, 56, 84]);
+// console.log(generateDiagonal(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// console.log(generateDiagonal(3, 7), [1, 4, 10, 20, 35, 56, 84]);
