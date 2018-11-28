80f8fe65a4013050d7675c1c0a84f20658038dfe
stef00 <stefanogechele@gmail.com>
2018-11-28 19:25:43 +0100

    fine

--- PascalsDiagonals.js
+++ PascalsDiagonals.js
@@ -1,7 +1,25 @@
-function generateDiagonal(n, l) {
+function generateDiagonal(row, len) {
     // return an array containing the numbers in the nth diagonal of Pascal's triangle, to the specified length
+    let n = row;
+    console.log(n);
+    let arr =[];
+
+    for (let k = 0; k < len; k++) {
+        let num = rFact(n)/(rFact(k)*rFact(n-k));
+        n++;
+        arr.push(num);
+        console.log(num);
+    }
+    return arr;
 }
 
+function rFact(num) {
+    if (num <= 0) {
+        return 1;
+    } else {
+        return num * rFact(num - 1);
+    }
+}
 
 
 console.log(generateDiagonal(2, 5), [1, 3, 6, 10, 15]);
