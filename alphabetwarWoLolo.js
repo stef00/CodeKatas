3f37a033d9073948360ffd4c8992be3a57245e62
stef00 <stefanogechele@gmail.com>
2018-12-01 21:18:58 +0100

    inizio

---
+++ alphabetwarWoLolo.js
@@ -0,0 +1,17 @@
+function alphabetWar(fight)
+{
+    return 'Wo lo loooooo';
+}
+
+
+console.log( alphabetWar("z") , "Right side wins!" );
+console.log( alphabetWar("tz") , "Left side wins!" );
+console.log( alphabetWar("jz") , "Right side wins!" );
+console.log( alphabetWar("zt") , "Left side wins!" );
+console.log( alphabetWar("sj") , "Right side wins!" ); 
+console.log( alphabetWar("azt") , "Left side wins!" );
+console.log( alphabetWar("tzj") , "Right side wins!" );
+console.log( alphabetWar("jbdt") , "Let's fight again!" );
+console.log( alphabetWar("wololooooo") , "Left side wins!" );
+console.log( alphabetWar("zdqmwpbs") , "Let's fight again!" );
+console.log( alphabetWar("ztztztzs") , "Left side wins!" );
