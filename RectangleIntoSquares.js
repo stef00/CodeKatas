1ae65dc196bd76f93d7463811ce0e89574af8b87
stef00 <stefanogechele@gmail.com>
2018-12-15 15:47:27 +0100

    inizio

---
+++ RectangleIntoSquares.js
@@ -0,0 +1,9 @@
+function sqInRect(lng, wdth) {
+    //your code here
+}
+
+
+console.log(sqInRect(5, 5), null)
+console.log(sqInRect(5, 3), [3, 2, 1, 1])
+console.log(sqInRect(3, 5), [3, 2, 1, 1])
+console.log(sqInRect(20, 14), [14, 6, 6, 2, 2, 2])
